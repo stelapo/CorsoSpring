0fa2ba8bb162b77e46f1b4f77c13907e0ba66c4f
Lapo Pancani <lapo.pancani@3esse.retexspa.com>
2021-12-01 12:29:08 +0100

    Aggiunto aspect con annotation e non con xml

--- src/com/corso/aop/EsempioAOPAnnotation.java
+++ src/com/corso/aop/EsempioAOPAnnotation.java
@@ -1,4 +1,39 @@
 package com.corso.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
 public class EsempioAOPAnnotation {
+
+    public void EsempioAOPAnnotation(){
+        System.out.println("EsempioAOPAnnotation NEW" );
+    }
+
+    @Pointcut("execution(* com.corso.figure.*.* (..))") //&& args(lato)
+    public void pointCutUnico(){}
+
+    @Before("pointCutUnico()")
+    public void beforeAdvice( JoinPoint jp) {
+        System.out.println("EsempioAOPAnnotation beforeAdvice: prima del metodo :" + jp.getSignature() );
+    }
+
+    public void afterReturningAdvice( JoinPoint jp, Object result) {
+        System.out.println("EsempioAOPAnnotation afterReturningAdvice: dopo il metodo :" + jp.getSignature() + " result = " + result );
+    }
+
+    public void afterThrowingAdvice(JoinPoint jp, Throwable error) throws Throwable {
+        System.out.println("EsempioAOPAnnotation afterThrowingAdvice: dopo l'eccezione :" + jp.getSignature() + " error=" + error.getMessage());
+
+    }
+
+    public Object aroundAdvice(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("EsempioAOPAnnotation aroundAdvice: around 1 del metodo:" + jp.getSignature() );
+        Object o = jp.proceed();
+        System.out.println("EsempioAOPAnnotation aroundAdvice: around 2 del metodo:" + jp.getSignature() );
+        return o;
+    }
 }
--- src/com/corso/figure/test/AOPtest.java
+++ src/com/corso/figure/test/AOPtest.java
@@ -1,28 +1,27 @@
 package com.corso.figure.test;
 
+import com.corso.figure.Figura;
+import com.corso.figure.Quadrato;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.corso.figure.*;
-
 
 
 public class AOPtest {
 
-	public static void main(String[] args) {
-		 ConfigurableApplicationContext factory = new ClassPathXmlApplicationContext("beans.xml");
-		  factory.registerShutdownHook(); // this should be it, destroys are called before your app exits		 
-		
-		//    Resource resource=new ClassPathResource("beans.xml");  
-		//    BeanFactory factory=new XmlBeanFactory(resource);  
-		 
-		 
-		 Figura c1 =(Figura)factory.getBean("figura02");
-		
-		 Double area = c1.area();
-		 
-		 System.out.println("area finale = " + area);
-		 
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext factory = new ClassPathXmlApplicationContext("beans.xml");
+        factory.registerShutdownHook(); // this should be it, destroys are called before your app exits
+
+        //    Resource resource=new ClassPathResource("beans.xml");
+        //    BeanFactory factory=new XmlBeanFactory(resource);
+
+        Figura c1 = (Figura) factory.getBean("figura02");
+
+        Double area = c1.area();
+
+        System.out.println("area finale = " + area);
+
+    }
 
 }
